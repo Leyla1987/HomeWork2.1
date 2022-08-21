6629c2e650e5275ea44ff8bae4292beae45f8e22
Leyla1987 <glenmorangie@list.ru>
2022-08-21 20:06:39 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Пишу свое ДЗ!");
         System.out.println("Делаю коммит и пуш!");
-        System.out.println("Отправляю на гидхаб!");
+        System.out.println("Отправляю на гидхаб");
     }
 }
